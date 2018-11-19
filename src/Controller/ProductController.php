22df02e82c321a4915b9b3ba4e9b1a5565af6ec5
Asia <finewitch@gmail.com>
2018-11-19 16:04:43 +0100

    setup

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="product")
+     */
+    public function index()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(1999);
+
+        $entityManager->persist($product);
+
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+
+    }
+}
---
+++ src/Controller/ProductShowController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Product;
+
+class ProductShowController extends AbstractController
+{
+    /**
+     * @Route("/product/{id}", name="product_show")
+     */
+    public function show()
+    {
+            $product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great product: '.$product->getName());
+
+    }
+}
