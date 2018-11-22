b2a4be9311344c7b3ac450cfa859926a316b7f35
Asia <finewitch@gmail.com>
2018-11-22 18:04:58 +0100

    up

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -20,23 +20,26 @@ use Slince\Shopify\Client;
 
 class ProductController extends AbstractController
 {
+
     /**
      * @Route("/product", name="product")
      */
+
     public function index(Request $request)
     {
         $encoders = array(new XmlEncoder(), new JsonEncoder());
         $normalizers = array(new ObjectNormalizer());
         
         $serializer = new Serializer($normalizers, $encoders);
- 
 
         $data = json_decode($request->getContent(), true);
+
+        $request = Request::createFromGlobals();
 
         var_dump($data['produkt_type']);
 
 
-
+        //api,pass,secret
         $credential = new PrivateAppCredential('e34e44297a9aec24a869b64147e0b17e', 'f17acfadd7a7528e00c70690e6fd452e', 'b868d483e4667c0118e358ef10e73fb5');
 
         $client  = new Client($credential, 'textil-one-dev.myshopify.com', [
@@ -51,18 +54,23 @@ class ProductController extends AbstractController
         ]);
 
 
-        
-        // $catIds = array_map(create_function('$o', 'return $o->id;'), $products);
-        // var_dump($catIds);
         $smallProducts = [];
+        
         
         foreach ($products as $value) {
             
             $smallProducts[] = [
                 'id' => $value->getId(),
-                'title' => $value->getTitle()
+                'title' => $value->getTitle(),
+                'handle' => $value->getHandle(),
+                'image' => $value->getImage()->getSrc(),
+                'price' => $value->getVariants()[0]->getPrice()
             ];
+
         } 
+
+        //ZAPISYWANIE DO METAFIELDS
+
 
     
         $filteredProducts = $serializer->serialize($smallProducts, "json");
