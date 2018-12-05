ac9484d83720333c1c606264d1303fc72e6de63a
Joanna <finewitch@gmail.com>
2018-12-05 13:28:57 +0100

    Update ProductController.php

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -25,7 +25,7 @@ use App\Service\FilterProducts;
 use App\Service\SaveMetafields;
 
 
-class ProductController extends AbstractController
+class WizardController extends AbstractController
 {
 
     /**
@@ -34,39 +34,39 @@ class ProductController extends AbstractController
 
     public function new(FilterProducts $filterProducts, Request $request, SaveMetafields $saveMetafields)
     {
-        $responseProducts = [
-            'status' => 'ok',
-            'products' => $responseFilterProducts = $filterProducts->getAllProducts($request),
-        ];
-        // try {
-        // }
-        // catch (\Exception $e) { 
-        //         $response = [
-        //             'status' => 'error',
-        //             'msg' => $e->getMessage()
-        //         ];
-        // }
-        // if($response['status'] == 'ok') {
-        //     try {
-        //         $responseMetafields = $saveMetafields->index(json_decode($responseFilterProducts));
-        //        }
-        //     catch (\Exception $e) { 
-        //             $response = [
-        //                 'status' => 'error',
-        //                 'msg' => $e->getMessage(),
-        //             ];
-        //     }
-        // }
+        try {
+            $responseProducts = [
+                'status' => 'ok',
+                'products' => $responseFilterProducts = $filterProducts->getAllProducts($request),
+            ];
+        }
+        catch (\Exception $e) { 
+                $responseProducts = [
+                    'status' => 'error',
+                    'msg' => $e->getMessage()
+                ];
+        }
+
+        if($responseProducts['status'] == 'ok' && count($responseProducts['products']) > 0 ) {
+            // dump($responseFilterProducts);exit;
+            try {
+                $responseMetafields = $saveMetafields->index(json_decode($responseFilterProducts));
+
+               }
+            catch (\Exception $e) { 
+                    $responseProducts = [
+                        'status' => 'error',
+                        'msg' => $e->getMessage(),
+                    ];
+            }
+        }
+        
         $response = new JsonResponse(
             $responseProducts,
             Response::HTTP_OK,
             array('content-type' => 'text/html')
         );
 
-        // $response->headers->set("Access-Control-Allow-Origin", "*");
-        // $response->headers->set("Access-Control-Allow-Credentials", "true");
-        // $response->headers->set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-        // $response->headers->set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
         return $response;
         
